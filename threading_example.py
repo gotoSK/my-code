d380465cc480beed0a6d105fa5695d4c7f14f7a5
unknown <karkisugam34@gmail.com>
2024-12-14 21:20:34 +0545

    testing

--- threading_example.py
+++ threading_example.py
@@ -1,10 +1,13 @@
 import threading
 import time
 
+lock = threading.Lock()
+
 def thread_code(tid):
-    for i in range(0,4):
-        print(tid, ": ", i)
-        time.sleep(1);
+    with lock:
+        for i in range(0,4):
+            print(tid, ": ", i)
+            time.sleep(1);
 
 threading.Thread(target=thread_code, args=('t1',)).start()
 threading.Thread(target=thread_code, args=('t2',)).start()
