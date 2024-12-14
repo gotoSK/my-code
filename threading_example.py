bc30f911fe1441af20ff0dc9e846afe7367fc4fb
unknown <karkisugam34@gmail.com>
2024-12-14 21:19:25 +0545

    testing

--- threading_example.py
+++ threading_example.py
@@ -1,8 +1,10 @@
 import threading
+import time
 
 def thread_code(tid):
     for i in range(0,4):
         print(tid, ": ", i)
+        time.sleep(1);
 
 threading.Thread(target=thread_code, args=('t1',)).start()
 threading.Thread(target=thread_code, args=('t2',)).start()
