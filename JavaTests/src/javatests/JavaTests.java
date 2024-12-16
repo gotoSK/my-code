c5bf7d7613ace73f52cdc6d13b72a62310ab2021
unknown <karkisugam34@gmail.com>
2024-12-16 13:23:48 +0545

    inheritence

---
+++ JavaTests/src/javatests/JavaTests.java
@@ -0,0 +1,28 @@
+package javatests;
+
+class Parent{
+    public void sign(){
+        System.out.println("parent.");
+    }
+    public void name(){
+        System.out.println("Parent");
+    }
+}
+
+class Child extends Parent{
+    public void name(){
+        System.out.println("Child");
+    }
+}
+
+public class JavaTests {
+    
+    public static void main(String[] args) {
+        Parent p1 = new Parent();
+        Child c1 = new Child();
+        c1.name();
+        p1.name();
+        p1.sign();
+        c1.sign();
+    }   
+}
