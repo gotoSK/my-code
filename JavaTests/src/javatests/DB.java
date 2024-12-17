42a0b35d6f72b7d530eac7a993e80de69819de9e
unknown <karkisugam34@gmail.com>
2024-12-17 17:21:59 +0545

    db

--- JavaTests/src/javatests/DB.java
+++
@@ -1,7 +0,0 @@
-package javatests;
-
-public class DB {
-    public void display(){
-        System.out.println("hey yo!");
-    }    
-}
--- JavaTests/src/javatests/runProg.java
+++ JavaTests/src/javatests/runProg.java
@@ -1,13 +1,33 @@
 package javatests;
 
+import java.sql.*;
+
 public class runProg {
-    
-    public static void main(String[] args) {
+    public static void inheritance(){
         Parent p1 = new Parent();
         Child c1 = new Child();
         c1.name();
         p1.name();
         p1.sign();
-        c1.sign();
+        c1.sign();        
+    }
+    
+    public static void main(String[] args) {
+        try {
+            // load driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            // creating connection
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "root", "12345");
+
+            if(con.isClosed()){
+                System.out.println("Connection closed!");
+            }
+            else{
+                System.out.println("Connection created :)");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }   
 }
