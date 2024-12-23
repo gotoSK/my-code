88d71b0d35ade83a121e901576b50c0096741009
unknown <karkisugam34@gmail.com>
2024-12-23 12:28:15 +0545

    main

---
+++ swingapps/src/swingapps/SwingDemo.java
@@ -0,0 +1,11 @@
+package swingapps;
+
+public class SwingDemo {
+    public static void main(String[] args) {
+        // GUIControls.guiControls();
+        // Menu1.menu1();
+        // Menu2.menu2();
+        // Menu3.menu3();
+        Menu4.menu4();
+    }
+}
