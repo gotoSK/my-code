0b6613241631d3d8235ac9aa239031873c6d0e9b
unknown <karkisugam34@gmail.com>
2024-12-28 07:46:58 +0545

    4 recursions demo

---
+++ recursion.py
@@ -0,0 +1,10 @@
+from gcd import gcd
+from factorial import facto
+from fibonacci import fib
+from tower_of_hanoi import toh
+
+# Example usage
+print("GCD of 56 and 98:", gcd(56, 98))
+print("Factorial of 5:", facto(5))
+print("Fibonacci of 6:", fib(6))
+toh('S', 'T', 'A', 3)
