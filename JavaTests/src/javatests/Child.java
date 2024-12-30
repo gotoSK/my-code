609e78a6892cbe4921d60f9b33595fa968b3851e
unknown <karkisugam34@gmail.com>
2024-12-30 14:21:26 +0545

    java tests program

---
+++ JavaTests/src/javatests/Child.java
@@ -0,0 +1,7 @@
+package javatests;
+
+public class Child extends Parent{
+    Child(){
+        super();
+    }    
+}
---
+++ JavaTests/src/javatests/Parent.java
@@ -0,0 +1,10 @@
+package javatests;
+
+public class Parent {
+    Parent(){
+        System.out.println("Parent class called.");
+    }
+    public void getData(){
+        System.out.println("This is Parent Class.");
+    }  
+}
